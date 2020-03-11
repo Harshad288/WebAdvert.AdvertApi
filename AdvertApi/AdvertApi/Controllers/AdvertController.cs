813cc9fb4ca8dbda59a1fba5052b2da693d63c5b
Patil, Harshad <harshad.patil@siemens.com>
2020-03-11 20:47:12 +0530

    Added api code

---
+++ AdvertApi/AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,66 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201,Type= typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel advertModel)
+        {
+            string recordId = "";
+            try
+            {
+                 recordId = await _advertStorageService.Add(advertModel);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id= recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel advertModel)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(advertModel);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+            return new OkResult();
+        }
+     }
+}
---
+++ AdvertApi/AdvertApi/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
